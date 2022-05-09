66b611c67072b12784e8ef48b58578da7e530f21
GarryAmasia <garryamasia@outlook.com>
2022-05-09 16:48:16 +1000

    starting firebase

--- src/route/navigation/Navigation.js
+++ src/route/navigation/Navigation.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+// import { ReactComponent as AnchorLogo } from "../../assets/logo-anchor.png";
+import anchor from "../../assets/logo-anchor.png";
+import "./navigation.style.scss";
 
 export const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>logo</div>
+          <img src={anchor} alt="" className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="shop">
             Shop
+          </Link>
+          <Link className="nav-link" to="sign-in">
+            SIGN IN
           </Link>
         </div>
       </div>
